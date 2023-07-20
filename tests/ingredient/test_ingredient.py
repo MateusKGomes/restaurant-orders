39a4ca75454b19b51d327bf75d54b141825a6573
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-07-20 15:17:26 -0300

    Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,15 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient('tomate')
+    ingredient2 = Ingredient('salmão')
+    ingredient3 = Ingredient('tomate')
+
+
+    assert ingredient.restrictions == set()
+    assert ingredient.name == 'tomate'
+    assert ingredient.__repr__() == "Ingredient('tomate')"
+    assert ingredient.__eq__(ingredient2) is False
+    assert ingredient.__eq__(ingredient3) is True
+    assert ingredient.__hash__() == hash(ingredient)
+    assert ingredient2.__hash__() != hash(ingredient)
